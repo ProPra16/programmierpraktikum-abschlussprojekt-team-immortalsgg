a6971b90555e74679180318748b69d9342103911
altra <altra102@uni-duesseldorf.de>
2016-06-26 17:13:57 +0200

    hautpmenu schwarz gemacht und test weiß sieht ganz gut aus? können wa auch wieder ändern . hab ein bisschen mit css rumgespielt , aber nur in der fxml datei

--- src/main/java/de/hhu/imtgg/TDDTMain.java
+++ src/main/java/de/hhu/imtgg/TDDTMain.java
@@ -1,6 +1,8 @@
 package de.hhu.imtgg;
 
 import java.io.IOException;
+import java.net.URL;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -15,6 +17,7 @@ public class TDDTMain extends Application {
 	private static Stage primarystage;
 	private static BorderPane tddtviewlayout;
 	private static BorderPane tddtrainerview;
+	private static Scene scene;
 	
 	@Override
 	public void start(Stage stage) {
@@ -28,18 +31,19 @@ public class TDDTMain extends Application {
 		launch(args);
 	}
 	
-	public static  void initTDDTViewLayout() {			// laedt layout fuer startbild aus FXML -> siehe TDDTView.fmxl
+	public static void initTDDTViewLayout() {			// laedt layout fuer startbild aus FXML -> siehe TDDTView.fmxl
 		 try {
 			 FXMLLoader loader = new FXMLLoader();
 			 loader.setLocation(TDDTMain.class.getResource("layout/TDDTView.fxml"));
 			 tddtviewlayout = (BorderPane) loader.load(); 
-			 Scene scene = new Scene(tddtviewlayout);
+			 scene = new Scene(tddtviewlayout);			 
 			 primarystage.setScene(scene);
 			 primarystage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+	
 	
 	public static void initTDDTrainerView(String testcode,String sourcecode) { //komisch gemacht doch die textarea laesst sich veraendern beim start
 		try {
@@ -54,7 +58,8 @@ public class TDDTMain extends Application {
 			TextArea textareatestcode = (TextArea) anchorpanewithtestcodearea.getChildren().get(0); 
 			textareatestcode.setText(testcode);
 			textareasourcecode.setText(sourcecode);
-			Scene scene = new Scene(tddtrainerview);
+			scene = new Scene(tddtrainerview);
+						
 			primarystage.setScene(scene);
 			primarystage.show();
 		} catch(IOException e) {
